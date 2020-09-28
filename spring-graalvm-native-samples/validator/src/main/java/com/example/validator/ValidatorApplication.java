eb04d1acf6acda76e6e5c09f654ee03bbd2264ce
Dave Syer <dsyer@vmware.com>
2020-09-28 07:33:17 +0100

    Use a custom ConstraintValidatorFactory

--- spring-graalvm-native-samples/validator/src/main/java/com/example/validator/ValidatorApplication.java
+++ spring-graalvm-native-samples/validator/src/main/java/com/example/validator/ValidatorApplication.java
@@ -1,18 +1,27 @@
 package com.example.validator;
 
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.ConstraintValidatorFactory;
 import javax.validation.Validator;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
+import org.hibernate.validator.internal.constraintvalidators.bv.NotBlankValidator;
+import org.hibernate.validator.internal.constraintvalidators.bv.PatternValidator;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.validation.MessageInterpolatorFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication(proxyBeanMethods = false)
 @EnableConfigurationProperties(Foo.class)
@@ -28,7 +37,17 @@ public class ValidatorApplication {
 	}
 
 	@Bean
-	public static BeanDefinitionRegistryPostProcessor ripper() {
+	@ConditionalOnProperty(prefix = "org.graalvm.nativeimage", name = "imagecode")
+	public static LocalValidatorFactoryBean configurationPropertiesValidator() {
+		LocalValidatorFactoryBean factoryBean = new LocalValidatorFactoryBean();
+		factoryBean.setConstraintValidatorFactory(new CustomValidatorFactory());
+		MessageInterpolatorFactory interpolatorFactory = new MessageInterpolatorFactory();
+		factoryBean.setMessageInterpolator(interpolatorFactory.getObject());
+		return factoryBean;
+	}
+
+	@Bean
+	public static BeanDefinitionRegistryPostProcessor removeMethodValidationPostProcessor() {
 		return new BeanDefinitionRegistryPostProcessor() {
 
 			@Override
@@ -47,10 +66,39 @@ public class ValidatorApplication {
 
 }
 
+class CustomValidatorFactory implements ConstraintValidatorFactory {
+
+	@Override
+	@SuppressWarnings({ "unchecked", "rawtypes"})
+	public <T extends ConstraintValidator<?, ?>> T getInstance(Class<T> key) {
+
+		if (key == PatternValidator.class) {
+			return (T) new PatternValidator();
+		} else if (key == NotBlankValidator.class) {
+			return (T) new NotBlankValidator();
+		}
+
+		return (T) new ConstraintValidator() {
+
+			@Override
+			public boolean isValid(Object value, ConstraintValidatorContext context) {
+				return true;
+			}
+
+		};
+	}
+
+	@Override
+	public void releaseInstance(ConstraintValidator<?, ?> instance) {
+	}
+	
+}
+
 @ConfigurationProperties("app")
 @Validated
 class Foo {
 	@Pattern(regexp = "[A-Z][a-z]+", message = "Invalid lastname")
+	@NotBlank
 	private String value;
 
 	public Foo() {
